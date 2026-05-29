0811891f0df44994c331efd71639104f78fb2211
danhthetrung <danhthetrung24@gmail.com>
2026-05-29 14:27:49 +0700

    Bump asset version for checkin upload

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
  * Chỉnh nội dung landing page tại đây.
  */
 
-const APP_VERSION = '2026-05-18 why-section-card';
+const APP_VERSION = '2026-05-29 checkin-media-upload';
 
 return [
     'brandName' => 'NAM THẮNG TRAVEL BUS',
